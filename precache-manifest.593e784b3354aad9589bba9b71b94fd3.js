2ac2b08a2c3a3cf609a3c495742823f00a770719
charisma-smile <charisma-smile@users.noreply.github.com>
2022-07-27 06:11:00 +0000

    deploy: 7b5790d423d49889769ad7977e71a81d99f8f5f5

---
+++ precache-manifest.593e784b3354aad9589bba9b71b94fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "061d16927243bc4cb840",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.fea6ce30.chunk.css"
+  },
+  {
+    "revision": "061d16927243bc4cb840",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.061d1692.chunk.js"
+  },
+  {
+    "revision": "13ef54ff9430faa800a0",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.13ef54ff.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "02ccd87ce232ba1763492cff302bd6ab",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
